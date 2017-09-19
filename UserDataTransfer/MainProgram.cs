541ce15746fa4e6e09c88b436b17d8430d3c7e2c
kc0hvb <kc0hvb@gmail.com>
2017-09-19 15:57:38 -0600

    saving

--- UserDataTransfer/MainProgram.cs
+++ UserDataTransfer/MainProgram.cs
@@ -11,26 +11,36 @@ namespace UserDataTransfer
 {
     class MainProgram
     {
-        private DataTable dDataTable = new DataTable();
+        private DataTable dDataTableSource = new DataTable();
+        private DataTable dDataTableTarget = new DataTable();
         public void RunMainProgram(string pConnStringSource, string pConnStringTarget)
         {
             
         }
         public void PullData(string pConnStringSource, string pConnStringTarget)
         {
-            string connString = pConnStringSource;
             string query = "select * from table";
 
-            SqlConnection conn = new SqlConnection(connString);
-            SqlCommand cmd = new SqlCommand(query, conn);
-            conn.Open();
+            SqlConnection connSource = new SqlConnection(pConnStringSource);
+            SqlCommand cmdSource = new SqlCommand(query, connSource);
+            connSource.Open();
+
+            SqlConnection connTarget = new SqlConnection(pConnStringTarget);
+            SqlCommand cmdTarget = new SqlCommand(query, connTarget);
+            connTarget.Open();
 
             // create data adapter
-            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            SqlDataAdapter daSource = new SqlDataAdapter(cmdSource);
             // this will query your database and return the result to your datatable
-            da.Fill(dDataTable);
-            conn.Close();
-            da.Dispose();
+            daSource.Fill(dDataTableSource);
+            connSource.Close();
+            daSource.Dispose();
+
+            SqlDataAdapter daTarget = new SqlDataAdapter(cmdTarget);
+            // this will query your database and return the result to your datatable
+            daTarget.Fill(dDataTableTarget);
+            connTarget.Close();
+            daTarget.Dispose();
         }
 
     }
